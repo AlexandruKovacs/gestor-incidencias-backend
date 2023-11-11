d5f0023deb4064014d16798e81916609f1e461f4
Alex Kovacs <axkovacs27@gmail.com>
2023-11-11 17:19:18 +0100

    Configuración mapeo objeto relacional

--- src/Entity/AulaEstancia.php
+++ src/Entity/AulaEstancia.php
@@ -2,31 +2,17 @@
 
 namespace App\Entity;
 
-use App\Repository\AulaEstanciaRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: AulaEstanciaRepository::class)]
-#[ORM\Table(schema: 'public')]
 class AulaEstancia
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
     private ?string $nombre = null;
+
+    private ?string $descripcion = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
     }
 
     public function getNombre(): ?string
@@ -37,6 +23,18 @@ class AulaEstancia
     public function setNombre(string $nombre): static
     {
         $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): static
+    {
+        $this->descripcion = $descripcion;
 
         return $this;
     }
